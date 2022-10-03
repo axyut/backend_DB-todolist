852d6e9b37d31d96e2a515e3eecc8e2b895811b4
Achyut <aoogleak@gmail.com>
2022-10-03 14:29:28 +0545

    5.2 Major Changes

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();   
 
 app.set('view engine', 'ejs');   
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("CSS"));
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}); 
-const itemsSchema ={
+
+const itemsSchema = new mongoose.Schema({
     name: String
-};
+});
 const Item = mongoose.model("Item", itemsSchema);
 
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+});
+const List = mongoose.model("List", listSchema);
+
 const item1= new Item({
-    name:"heyy"
+    name:"Manage your tasks."
 });
 const item2= new Item({
-    name:"heyy heyy"
+    name:"Hit the + button to add a new item."
 });
 const item3= new Item({
-    name:"heyy heyy heyy"
+    name:"<-- Hit this to delete an item."
 });
 
 const defaultItems = [item1, item2, item3];
-
 
 app.get("/", function(req,res){
     Item.find({}, function(err, foundItems){
@@ -39,23 +46,70 @@ app.get("/", function(req,res){
             });   
             res.redirect("/");
         }else{
-        res.render("list", { newlistitem: Item});  // list is the ejs file name which has to exist inside views folder beacuse ejs automatically looks insidw views folder
+        res.render("list", {listTitle: "Today", newListItems: foundItems});  // list is the ejs file name which has to exist inside views folder beacuse ejs automatically looks insidw views folder
         }
     });    
 });
 
+app.get("/:customListName", function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, function(err, foundList){
+        if (!err) {
+            if(!foundList){  // Create a new list
+                const list = new List ({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            }else{          // Use Existing List
+                res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+            }
+        }
+    })
+})
+
 app.post("/", function(req,res){
 
     const itemName = req.body.newItem;
+    const listName = req.body.list;
+
     const item = new item({
         name: itemName
     })
-    item.save();
-
-    res.redirect("/");
+    if(listName === "Today"){
+        item.save();
+        res.redirect("/");
+    }else{
+        List.findOne({name: listName}, function(err, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
 
 });
 
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if(listName === "Today"){
+        Item.findByIdAndRemove(checkedItemId, function(err){
+            if(!err){
+                console.log("Sucessfully deleted checked item.");
+                res.redirect("/");
+            }
+        });
+    }else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+            if(!err){
+                res.redirect("/" + listName);
+            }
+        })
+    }
+})
 
 app.listen(8000, function(){
     console.log("SERVER IS RUNNING ON PORT 8000");
